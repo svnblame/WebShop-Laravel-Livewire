bc3e016e54dedd218c0e622f860fc6a986ac1bbb
Gene Kelley <phpcodehead@gmail.com>
2024-05-30 16:53:38 -0700

    Add cart item subtotals and cart totals to cart page. Add Checkout button if loggedin, if guest show links to register or login. Changed some styling.

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,15 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Money\Currency;
+use Money\Money;
 
 class Cart extends Model
 {
     use HasFactory;
 
     protected $fillable = ['session_id'];
+
+    protected function total(): Attribute
+    {
+        return Attribute::make(
+            get: function() {
+                return $this->items->reduce(function(Money $total, CartItem $item) {
+                    return $total->add($item->subtotal);
+                }, new Money(0, new Currency('USD')));
+            }
+        );
+    }
 
     public function items(): HasMany
     {
