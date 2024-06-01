4c82b187dd3793324cd704661a62a3f970a6f937
Gene Kelley <phpcodehead@gmail.com>
2024-06-01 15:22:08 -0700

    Add migrate from session to user cart. Add increment / decrement item quantities, items subtotal, and cart total functionality.

--- app/Actions/Webshop/AddProductVariantToCart.php
+++ app/Actions/Webshop/AddProductVariantToCart.php
@@ -7,12 +7,12 @@ use App\Models\Cart;
 
 class AddProductVariantToCart
 {
-    public function add($variantId): void
+    public function add($variantId, $quantity = 1, $cart = null): void
     {
-        CartFactory::make()->items()->firstOrCreate([
+        ($cart ?: CartFactory::make())->items()->firstOrCreate([
             'product_variant_id' => $variantId
         ], [
             'quantity' => 0,
-        ])->increment('quantity');
+        ])->increment('quantity', $quantity);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace App\Providers;
 
+use App\Actions\Webshop\MigrateSessionCart;
+use App\Factories\CartFactory;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
 use Money\Currencies\ISOCurrencies;
 use Money\Formatter\IntlMoneyFormatter;
 use Money\Money;
@@ -30,6 +36,15 @@ class AppServiceProvider extends ServiceProvider
             $moneyFormatter  = new IntlMoneyFormatter($numberFormatter, $currencies);
 
             return $moneyFormatter->format($money);
+        });
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->first();
+
+            if ($user && Hash::check($request->password, $user->password)) {
+                (new MigrateSessionCart)->migrate(CartFactory::make(), $user?->cart ?: $user->cart()->create());
+                return $user;
+            }
         });
     }
 }
